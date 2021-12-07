550889365e8def8e698a26ff10f2620cd6a748d7
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-12-07 14:57:18 -0700

    working on posts.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 import Posts from './components/Posts/Posts.js';
-import SearchBar from './components/SearchBar/SearchBar.js';
+// import SearchBar from './components/SearchBar/SearchBar.js';
 // Import the dummyData
 import postData from './dummy-data';
 import './App.css';
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Post from './Post';
 import './Posts.css';
 
+
+
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
@@ -9,7 +11,9 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {}
       {/* Check the implementation of Post to see what props it requires! */}
+
     </div>
   );
 };
